c435bceeb3800fcb8c30552e1462a9a7dc801350
Kelvin Jesus <kelvin.jesus@webkriativa.com.br>
2024-06-19 15:04:38 -0300

    Exercícios Programação

--- 03-projeto-de-funcoes-2024-06-07/exercicios/01.rkt
+++ 03-projeto-de-funcoes-2024-06-07/exercicios/01.rkt
@@ -2,7 +2,8 @@
 
 (require examples)
 
-;; 12)
+;; 13)
+;;
 ;; InteiroPositivo -> Boolean
 ;;
 ;; Produz #t se uma pessoa com idade id é isento da
@@ -18,7 +19,7 @@
 (define (isento-tarifa? id)
   (or (<= id 17) (>= id 65)))
 
-;; 13)
+;; 14)
 ;; Inteiro -> Inteiro
 ;;
 ;; Conta a quantidade de dígitos de n.
@@ -33,7 +34,7 @@
 (define (quantidade-digitos n)
   (string-length (number->string (abs n))))
 
-;; 14)
+;; 15)
 ;; InteiroPositivo -> Boolean
 ;;
 ;; Produz #t se uma pessoa com a idade n é supercentenária,
@@ -44,9 +45,9 @@
  (check-equal? (supercentenario 112) #t))
 
 (define (supercentenario n)
-  (or (>= n 110) #f))
+  (or (>= n 110)))
 
-;; 15)
+;; 16)
 ;; String -> String
 ;;
 ;; Transforma a data de entrada do formato "dia/mes/ano"
@@ -55,37 +56,37 @@
 ;; Requer que o dia e o mês tenham dois dígitos e que
 ;; o ano tenha quatro dígitos.
 (examples
-    (check-equal? (data-dma->data-amd "19/07/2023") "2023/07/19")
-    (check-equal? (data-dma->data-amd "01/01/1980") "1980/01/01")
-    (check-equal? (data-dma->data-amd "02/02/2002") "2002/02/02"))
+ (check-equal? (data-dma->data-amd "19/07/2023") "2023/07/19")
+ (check-equal? (data-dma->data-amd "01/01/1980") "1980/01/01")
+ (check-equal? (data-dma->data-amd "02/02/2002") "2002/02/02"))
 
 (define (data-dma->data-amd data)
-    (define dia (substring data 0 2))
-    (define mes (substring data 3 5))
-    (define ano (substring data 6 10))
-    (string-append ano "/" mes "/" dia))
+  (define dia (substring data 0 2))
+  (define mes (substring data 3 5))
+  (define ano (substring data 6 10))
+  (string-append ano "/" mes "/" dia))
 
-;; 16) Escreva a especificação para a seguinte implementação de função. 
-;; Observe que a especificação sozinha deve ser suficiente para um 
+;; 17) Escreva a especificação para a seguinte implementação de função.
+;; Observe que a especificação sozinha deve ser suficiente para um
 ;; desenvolvedor fazer uma implementação.
 ;;
-;; Inteiro -> Float
+;; Número -> Número
 ;;
 ;; Retorna o valor total após o aumento com base no valor e na porcentagem
 
 (examples
-  (check-equal? (aumenta 1000 10) 1100)
-  (check-equal? (aumenta 200 2) 204)
-  (check-equal? (aumenta 0 0) 0)
-  (check-equal? (aumenta 250 92))
+ (check-equal? (aumenta 1000.0 10.0) 1100.0)
+ (check-equal? (aumenta 200.0 2.0) 204.0)
+ (check-equal? (aumenta 0.0 0.0) 0.0)
+ (check-equal? (aumenta 250.0 92.0) 480.0))
 
 (define (aumenta valor porcentagem)
-    (* valor (+ 1 (/ porcentagem 100))))
+  (* valor (+ 1 (/ porcentagem 100))))
 
-;; 17) Escreva a especificação para a seguinte implementação de função. Observe 
-;; que a especificação sozinha deve ser suficiente para um programador 
+;; 18) Escreva a especificação para a seguinte implementação de função. Observe
+;; que a especificação sozinha deve ser suficiente para um programador
 ;; fazer uma implementação.
-;; 
+;;
 ;; String -> String
 ;;
 ;; Tamanhos
@@ -94,19 +95,137 @@
 ;; - "longo"
 ;;
 ;; Deve informar o tamanho do nome.
-;; Se a quantidade de caracteres do nome for menor ou igual a 4, produz "curto". 
+;; Se a quantidade de caracteres do nome for menor ou igual a 4, produz "curto".
 ;; Caso seja menor ou igual a 10, produz "médio".
 ;; Caso contrário, produz "longo".
 
 (examples
-  (check-equal? (tamanho-nome "Maria") "médio")
-  (check-equal? (tamanho-nome "Ana") "curto")
-  (check-equal? 
-    (tamanho-nome "Taumatawhakatangihangakoauauotamateaturipukakapikimaungahoronukupokaiwhenuakitanatahu") 
-    "longo"))
+ (check-equal? (tamanho-nome "Ana") "curto")
+ (check-equal? (tamanho-nome "Ruan") "curto")
+ (check-equal? (tamanho-nome "Maria") "médio")
+ (check-equal? (tamanho-nome "Nickolasen") "médio")
+ (check-equal? (tamanho-nome "Constantine") "longo")
+ (check-equal?
+  (tamanho-nome "Taumatawhakatangihangakoauauotamateaturipukakapikimaungahoronukupokaiwhenuakitanatahu")
+  "longo"))
 
 (define (tamanho-nome nome)
-    (cond
-        [(<= (string-length nome) 4) "curto"]
-        [(<= (string-length nome) 10) "médio"]
-        [else "longo"]))
+  (cond
+    [(<= (string-length nome) 4) "curto"]
+    [(<= (string-length nome) 10) "médio"]
+    [else "longo"]))
+
+;; 19) Projete uma função que adicione um ponto final a uma frase
+;; se ela não acabar com um.
+;;
+;; String frase -> String
+;;
+;; Adiciona ponto final a uma frase caso não tenha.
+;; Produz a frase com um ponto final caso não termine com ponto final.
+;; Caso contrário, devolve a mesma frase
+
+(examples
+ (check-equal? (adiciona-ponto-final "Vai Corinthians.") "Vai Corinthians.")
+ (check-equal? (adiciona-ponto-final "Vai Corinthians") "Vai Corinthians.")
+ (check-equal? (adiciona-ponto-final ".") ".")
+ (check-equal? (adiciona-ponto-final "") "."))
+
+(define (adiciona-ponto-final frase)
+  (define tamanho-frase (string-length frase))
+  (cond
+    [(equal? tamanho-frase 0) (string-append frase ".")]
+    [(equal? (substring frase (sub1 tamanho-frase)) ".") frase]
+    [else (string-append frase ".")]))
+
+;; 20) Projete uma função que determine se uma palavra tem um traço ("-")
+;; no meio, como por exemplo, "lero-lero". Não use nenhuma
+;; condicional na implementação.
+;;
+;; String palavra -> Boolean
+;;
+;; Produz #t se a palavra contém um "-" exatamente no meio.
+;; Caso contrário, produz #f
+
+(examples
+ (check-equal? (tem-traco-meio? "lero-lero") #t)
+ (check-equal? (tem-traco-meio? "tem-traco") #f)
+ (check-equal? (tem-traco-meio? "-") #t)
+ (check-equal? (tem-traco-meio? "") #f))
+
+(define (tem-traco-meio? palavra)
+  (define tamanho-palavra (string-length palavra))
+  (define meio (quotient tamanho-palavra 2))
+  (and (odd? tamanho-palavra)
+       (equal? (substring palavra meio (add1 meio)) "-")))
+
+;; 21) Projete uma função que encontre o máximo entre
+;; três números dados. Não use a função pré-definida max.
+;;
+;; Número Número Número -> Número
+;;
+;; Deve devolver o maior entre os valores x, y e z. Sem utilizar a função max
+
+(examples
+ (check-equal? (maximo 30 20 10) 30)
+ (check-equal? (maximo 34 280 38) 280)
+ (check-equal? (maximo 32 69 999) 999)
+ (check-equal? (maximo 99 99 99) 99))
+
+(define (maximo x y z)
+  (cond
+    [(and (>= x y) (>= x z)) x]
+    [(>= y z) y]
+    [else z]))
+
+;; 22) Projete uma função que receba como parâmetro uma string e
+;; um número natural n e substitua os primeiros n caracteres
+;; da string por n letras x.
+
+;; 23) Faça a execução passo a passo de algumas funções definidas nos
+;; exercícios. Para isto você deve criar um novo arquivo,
+;; mudar a linguagem para “Beginning Student” e copiar o código da função e da
+;; chamada exemplo da função.
+
+;; 24) Você está fazendo um programa e precisa verificar se um
+;; texto digitado pelo usuário está de acordo com algumas regras.
+;; A regra “sem espaços extras” requer que o texto não comece e
+;; não termine com espaços. Projete uma função que verifique
+;; se um texto qualquer está de acordo com a regra “sem
+;; espaços extras”.
+
+;; String texto -> Boolean
+;;
+;; Produz #t se o texto seguir a regra de não possuir espaço no início e no fim.
+;; Caso contrário, devolve #f
+
+(examples
+ (check-equal? (sem-espacos-extras? "não tem") #t)
+ (check-equal? (sem-espacos-extras? " tem ") #f)
+ (check-equal? (sem-espacos-extras? " tem no início") #f)
+ (check-equal? (sem-espacos-extras? "tem no fim ") #f)
+ (check-equal? (sem-espacos-extras? "") #t)
+ (check-equal? (sem-espacos-extras? " ") #f))
+
+(define (sem-espacos-extras? texto)
+  (define tamanho (string-length texto))
+  (define ultima-pos (sub1 tamanho))
+  (define inicio (if (equal? tamanho 0)
+                     ""
+                     (substring texto 0 1)))
+  (define fim (if (equal? tamanho 0)
+                  ""
+                  (substring texto ultima-pos (add1 ultima-pos))))
+  (and (not (equal? inicio " "))
+      (not (equal? fim " "))))
+
+;(define (sem-espacos-extras? texto)
+;  (define tamanho (string-length texto))
+;  (define ultima-pos (sub1 tamanho))
+;  (define inicio (if (equal? tamanho 0)
+;                     ""
+;                     (substring texto 0 1)))
+;  (define fim (if (equal? tamanho 0)
+;                  ""
+;                  (substring texto ultima-pos (add1 ultima-pos))))
+;  (and (not (equal? inicio " "))
+;      (not (equal? fim " "))))
